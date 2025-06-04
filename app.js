a38a48e759f7b956d2c99f66dc7b91872c971924
HYEGYEONG <khkshs@gmail.com>
2025-06-04 18:03:16 +0900

    study

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express') ;
+const indexRouter=require("./routes/index")
+const app=express();
+const port = 8000;
+
+//app.set('view engine', 'ejs');
+//app.use('/views', express.static(__dirname+'views'));
+//app.use('/static', express.static(__dirname+'/static'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use('/',indexRouter);
+
+app.listen(port, ()=>{
+  console.log(`http://localhost:${port}`);
+});
